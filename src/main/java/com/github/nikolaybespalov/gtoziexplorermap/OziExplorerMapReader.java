e12d1dc95e2b54d11fdf0079ed1661f0a5234092
Nikolay Bespalov <nikolaybespalov@gmail.com>
2018-06-21 21:45:01 +0300

    Experiments

--- src/main/java/com/github/nikolaybespalov/gtoziexplorermap/OziExplorerMapReader.java
+++ src/main/java/com/github/nikolaybespalov/gtoziexplorermap/OziExplorerMapReader.java
@@ -4,130 +4,70 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.geotools.coverage.grid.GeneralGridEnvelope;
 import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.coverage.grid.GridGeometry2D;
+import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
-import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.data.DataSourceException;
 import org.geotools.data.WorldFileWriter;
 import org.geotools.gce.image.WorldImageReader;
-import org.geotools.gce.image.WorldImageWriter;
 import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.Envelope2D;
+import org.geotools.geometry.GeneralDirectPosition;
 import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
-import org.geotools.referencing.operation.matrix.GeneralMatrix;
 import org.geotools.referencing.operation.transform.AffineTransform2D;
 import org.geotools.referencing.operation.transform.ProjectiveTransform;
 import org.opengis.coverage.grid.Format;
-import org.opengis.geometry.DirectPosition;
-import org.opengis.geometry.Envelope;
+import org.opengis.geometry.BoundingBox;
 import org.opengis.parameter.GeneralParameterValue;
-import org.opengis.parameter.ParameterValue;
 import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.ReferenceIdentifier;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
-public class OziExplorerMapReader extends AbstractGridCoverage2DReader {
+public final class OziExplorerMapReader extends AbstractGridCoverage2DReader {
+    private MathTransform world2Model;
+    private WorldImageReader worldImageReader;
 
-    public OziExplorerMapReader(Path path) throws DataSourceException {
+    public OziExplorerMapReader(Path path) throws DataSourceException, FactoryException, TransformException {
         super(path);
 
-        this.coverageName = "Demo1";
+        Path imageFilePath = Paths.get("c:\\Program Files\\OziExplorer\\Maps\\Demo1.bmp");
 
-        double xMin = -26.858472, yMin = 152.288056;
-        double xMax = -26.449320, yMax = 152.991764;
-
-        double xPixelSize = xMax - xMin, yPixelSize = yMax - yMin, xULC = xMin, yULC = yMin;
-        Rectangle actualDim = new Rectangle(0, 0, 1202, 778);
+        // разбор .MAP-файла
+        // выделение информации о проекции
+        // выделение информации о преобразовании растр->модель
 
         this.crs = DefaultGeographicCRS.WGS84;
-        this.originalGridRange = new GeneralGridEnvelope(actualDim);
-        AffineTransform tt = new AffineTransform2D(xPixelSize, 0, 0, yPixelSize, xULC, yULC);
 
-        GeneralMatrix gm = new GeneralMatrix(new double[][]{
-                {xPixelSize, 0, xULC},
-                {0, yPixelSize, yULC},
-                {0, 0, 0}});
-        this.raster2Model = ProjectiveTransform.create(gm);
+        class Gcp {
+            DirectPosition2D xy;
+            Point linePixel;
 
-        try {
-            originalEnvelope = CRS.transform(ProjectiveTransform.create(gm), new GeneralEnvelope(actualDim));
-        } catch (TransformException e) {
-            throw new DataSourceException(e);
-        }
-
-        originalEnvelope.setCoordinateReferenceSystem(crs);
-
-        try {
-            Path wldPath = Paths.get(path.getParent().toString(), coverageName + ".wld");
-
-            if (wldPath.toFile().exists()) {
-                Files.delete(Paths.get(path.getParent().toString(), coverageName + ".wld"));
+            public Gcp(DirectPosition2D xy, Point linePixel) {
+                this.xy = xy;
+                this.linePixel = linePixel;
             }
-
-            Files.createFile(wldPath);
-
-
-            WorldFileWriter writer = new WorldFileWriter(wldPath.toFile(), tt);
-        } catch (IOException e) {
-            throw new DataSourceException(e);
-        }
-        //this.originalEnvelope = raster2Model.transform()
-        //this.originalEnvelope = new GeneralEnvelope(new double[]{xMin, yMin}, new double[]{xMax, yMax});
-//        final GeneralMatrix gm = new GeneralMatrix(3);
-//
-//        // compute an "offset and scale" matrix
-//        gm.setElement(0, 0, xPixelSize);
-//        gm.setElement(1, 1, yPixelSize);
-//        gm.setElement(0, 1, rotationX);
-//        gm.setElement(1, 0, rotationY);
-//        gm.setElement(0, 2, xULC);
-//        gm.setElement(1, 2, yULC);
-//        this.raster2Model = ProjectiveTransform.create(new GeneralMatrix());
-        //originalEnvelope = CRS.transform(CRS.findMathTransform(DefaultCartesianCS.GENERIC_2D, ), new GeneralEnvelope(actualDim));
-        //originalEnvelope.setCoordinateReferenceSystem(crs);
-
-        MathTransform latLon2Model;
-
-        try {
-            latLon2Model = CRS.findMathTransform(DefaultGeographicCRS.WGS84, crs, true);
-
-            double a = 152.991764 - 152.288056;
-            double b = -26.858472 - -26.449320;
-
-            AffineTransform t = AffineTransform.getScaleInstance(1202 / b, 778 / a);
-
-            AffineTransform t2 = AffineTransform.getTranslateInstance(26.449320, -152.288056);
-
-            AffineTransform t1 = new AffineTransform();
-            t1.concatenate(t);
-            t1.concatenate(t2);
-
-            Point2D p = t2.createInverse().transform(new DirectPosition2D(0, 0), null);
-
-            int asd = 0;
-            int asdf = asd;
-        } catch (FactoryException | NoninvertibleTransformException e) {
-            return;
         }
 
-        AffineTransform.getScaleInstance(1202 , 778 ).concatenate(
-                AffineTransform.getTranslateInstance(26.449320, -152.288056)
-        );
+        List<Gcp> gcps = new ArrayList<>();
+
+        Rectangle gridRange = new Rectangle();
+        GeneralEnvelope envelope = new GeneralEnvelope(2);
 
         try (Stream<String> lines = Files.lines(path, Charset.forName("windows-1251"))) {
             lines.forEach(line -> {
@@ -143,7 +83,55 @@ public class OziExplorerMapReader extends AbstractGridCoverage2DReader {
                     return;
                 }
 
-                if (key.startsWith("Point")) {
+                if (key.startsWith("Map Projection")) {
+                    try {
+                        String name = values[1];
+
+                        if (StringUtils.isEmpty(name)) {
+                            return;
+                        }
+
+                        switch (name) {
+                            case "Latitude/Longitude":
+                                this.crs = DefaultGeographicCRS.WGS84;
+                                break;
+                            default:
+                                return;
+                        }
+
+                        world2Model = CRS.findMathTransform(DefaultGeographicCRS.WGS84, this.crs, true);
+                    } catch (FactoryException e) {
+                        return;
+                    }
+                } else if (key.startsWith("MMPXY")) {
+                    if (values.length < 4) {
+                        return;
+                    }
+
+                    if (!NumberUtils.isCreatable(values[2]) || !NumberUtils.isCreatable(values[3])) {
+                        return;
+                    }
+
+                    Point borderPoint = new Point(NumberUtils.toInt(values[2]), NumberUtils.toInt(values[3]));
+
+                    gridRange.add(borderPoint);
+                } else if (key.startsWith("MMPLL")) {
+                    if (values.length < 4) {
+                        return;
+                    }
+
+                    if (!NumberUtils.isCreatable(values[2]) || !NumberUtils.isCreatable(values[3])) {
+                        return;
+                    }
+
+                    // transform
+
+                    DirectPosition2D borderPosition = new DirectPosition2D(NumberUtils.toDouble(values[2]), NumberUtils.toDouble(values[3]));
+
+
+
+                    envelope.add(borderPosition);
+                } else if (key.startsWith("Point")) {
                     if (values.length < 17) {
                         return;
                     }
@@ -152,13 +140,15 @@ public class OziExplorerMapReader extends AbstractGridCoverage2DReader {
                         return;
                     }
 
-                    double gcpY;
-                    double gcpX;
+                    Point gcpLinePixel = new Point(NumberUtils.toInt(values[2]), NumberUtils.toInt(values[3]));
+
+                    DirectPosition2D gcpXY = new DirectPosition2D(this.crs);
 
                     if (NumberUtils.isCreatable(values[6]) && NumberUtils.isCreatable(values[7]) &&
-                            NumberUtils.isCreatable(values[9]) && NumberUtils.isCreatable(values[10])) {
-                        double lat = (NumberUtils.toDouble(values[6]) + NumberUtils.toDouble(values[7])) / 60.0;
-                        double lon = (NumberUtils.toDouble(values[9]) + NumberUtils.toDouble(values[10])) / 60.0;
+                            NumberUtils.isCreatable(values[9]) && NumberUtils.isCreatable(values[10]) &&
+                            world2Model != null) {
+                        double lat = NumberUtils.toDouble(values[6]) + NumberUtils.toDouble(values[7]) / 60.0;
+                        double lon = NumberUtils.toDouble(values[9]) + NumberUtils.toDouble(values[10]) / 60.0;
 
                         if (values[8].equals("S")) {
                             lat = -lat;
@@ -168,31 +158,88 @@ public class OziExplorerMapReader extends AbstractGridCoverage2DReader {
                             lon = -lon;
                         }
 
-                        try {
-                            DirectPosition p = latLon2Model.transform(new DirectPosition2D(DefaultGeographicCRS.WGS84, lon, lat), null);
+                        DirectPosition2D gcpLatLon = new DirectPosition2D(DefaultGeographicCRS.WGS84, lat, lon);
 
-                            gcpX = p.getOrdinate(0);
-                            gcpY = p.getOrdinate(1);
+                        try {
+                            world2Model.transform(gcpLatLon, gcpXY);
                         } catch (TransformException e) {
                             return;
                         }
                     } else if (NumberUtils.isCreatable(values[14]) && NumberUtils.isCreatable(values[15])) {
-                        gcpY = NumberUtils.toDouble(values[15]);
-                        gcpX = NumberUtils.toDouble(values[14]);
+                        gcpXY.setLocation(NumberUtils.toDouble(values[14]), NumberUtils.toDouble(values[15]));
                     } else {
                         return;
                     }
 
-                    double gcpPixel = NumberUtils.toDouble(values[2]);
-                    double gcpLine = NumberUtils.toDouble(values[3]);
-
+                    gcps.add(new Gcp(gcpXY, gcpLinePixel));
                 }
-
-
             });
         } catch (IOException e) {
             throw new DataSourceException(e);
         }
+
+//        Gcp minGcp = gcps.get(0);
+//        Gcp maxGcp = gcps.get(1);
+
+        if (gcps.size() < 2) {
+            // TODO: Parse MMPXY and MMPLL
+        } else {
+//            Gcp minGcpX = gcps.stream().min(Comparator.comparingDouble(a -> a.linePixel.x)).get();
+//            Gcp maxGcpX = gcps.stream().max(Comparator.comparingDouble(a -> a.linePixel.x)).get();
+//
+//            Gcp minGcpY = gcps.stream().min(Comparator.comparingDouble(a -> a.linePixel.y)).get();
+//            Gcp maxGcpY = gcps.stream().max(Comparator.comparingDouble(a -> a.linePixel.y)).get();
+//
+//            minGcp = new Gcp(new DirectPosition2D(minGcpX.xy.getCoordinateReferenceSystem(), minGcpX.xy.x, minGcpY.xy.y), new Point(minGcpX.linePixel.x, minGcpY.linePixel.y));
+//            maxGcp = new Gcp(new DirectPosition2D(maxGcpX.xy.getCoordinateReferenceSystem(), maxGcpX.xy.x, maxGcpY.xy.y), new Point(maxGcpX.linePixel.x, maxGcpY.linePixel.y));
+        }
+
+        if (gridRange.isEmpty() || envelope.isEmpty()) {
+            return;
+        }
+
+        double xPixelSize = (envelope.getMaximum(0) - envelope.getMinimum(0)) / gridRange.width, yPixelSize = (envelope.getMaximum(1) - envelope.getMinimum(1)) / gridRange.height, xULC = envelope.getMinimum(0), yULC = envelope.getMinimum(1);
+
+        AffineTransform tt = new AffineTransform2D(xPixelSize, 0, 0, yPixelSize, xULC, yULC);
+
+        Point2D p = tt.transform(new Point2D.Double(1202, 778), null);
+
+        try {
+            originalEnvelope = CRS.transform(ProjectiveTransform.create(tt), new GeneralEnvelope(new Rectangle(0, 0, 1202, 778)));
+            originalEnvelope.setCoordinateReferenceSystem(crs);
+        } catch (TransformException e) {
+            throw new DataSourceException(e);
+        }
+
+        try {
+            Path wldPath = Paths.get(path.getParent().toString(), "Demo1" + ".wld");
+
+            if (wldPath.toFile().exists()) {
+                Files.delete(Paths.get(path.getParent().toString(), "Demo1" + ".wld"));
+            }
+
+            Files.createFile(wldPath);
+
+            WorldFileWriter writer = new WorldFileWriter(wldPath.toFile(), tt);
+
+            Path prjPath = Paths.get(path.getParent().toString(), "Demo1" + ".prj");
+
+            if (prjPath.toFile().exists()) {
+                Files.delete(Paths.get(path.getParent().toString(), "Demo1" + ".prj"));
+            }
+
+            Files.createFile(prjPath);
+
+            final FileWriter prjWriter = new FileWriter(prjPath.toFile());
+
+            prjWriter.write(crs.toWKT());
+
+            prjWriter.close();
+        } catch (IOException e) {
+            throw new DataSourceException(e);
+        }
+
+        worldImageReader = new WorldImageReader(imageFilePath.toFile());
 
 
         int asd = 0;
@@ -213,43 +260,6 @@ public class OziExplorerMapReader extends AbstractGridCoverage2DReader {
 
     @Override
     public GridCoverage2D read(GeneralParameterValue[] params) throws IllegalArgumentException, IOException {
-        if (params == null) {
-            return null;
-        }
-
-        GeneralEnvelope requestedEnvelope = null;
-        Rectangle dim = null;
-
-        for (int i = 0; i < params.length; i++) {
-            final ParameterValue param = (ParameterValue) params[i];
-            final ReferenceIdentifier name = param.getDescriptor().getName();
-            if (name.equals(AbstractGridFormat.READ_GRIDGEOMETRY2D.getName())) {
-                final GridGeometry2D gg = (GridGeometry2D) param.getValue();
-                requestedEnvelope = new GeneralEnvelope((Envelope) gg.getEnvelope2D());
-                dim = gg.getGridRange2D().getBounds();
-                continue;
-            }
-        }
-
-        if (requestedEnvelope == null) {
-            return null;
-        }
-
-
-        return null;
-
-//        Rectangle sourceRegion = new Rectangle(x, y, w, h); // The region you want to extract
-//
-//        ImageInputStream stream = ImageIO.createImageInputStream(input); // File or input stream
-//        Iterator<ImageReader> readers = ImageIO.getImageReaders(stream);
-//
-//        if (readers.hasNext()) {
-//            ImageReader reader = readers.next();
-//            reader.setInput(stream);
-//
-//            param.setSourceRegion(sourceRegion); // Set region
-//
-//            BufferedImage image = reader.read(0, param); // Will read only the region specified
-//        }
+        return worldImageReader.read(params);
     }
 }
--- src/test/java/com/github/nikolaybespalov/gtoziexplorermap/OziExplorerMapReaderTest.java
+++ src/test/java/com/github/nikolaybespalov/gtoziexplorermap/OziExplorerMapReaderTest.java
@@ -1,22 +1,32 @@
 package com.github.nikolaybespalov.gtoziexplorermap;
 
 import org.geotools.coverage.grid.GeneralGridEnvelope;
+import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.junit.Test;
 import org.opengis.parameter.GeneralParameterValue;
 import org.opengis.parameter.ParameterValue;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
 
 import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.assertEquals;
+
 public class OziExplorerMapReaderTest {
 
     @Test
-    public void test1() throws IOException {
-        OziExplorerMapReader reader = new OziExplorerMapReader(Paths.get("c:\\Users\\Nikolay Bespalov\\Documents\\github.com\\nikolaybespalov\\gt-oziexplorermap\\src\\test\\resources\\Maps\\Demo1.map"));
+    public void test1() throws IOException, FactoryException, TransformException {
+        OziExplorerMapReader reader = new OziExplorerMapReader(Paths.get("c:\\Program Files\\OziExplorer\\Maps\\Demo1.map"));
+
+        assertEquals(DefaultGeographicCRS.WGS84, reader.getCoordinateReferenceSystem());
+        //assertEquals(new GridEnvelope2D(new Rectangle(0, 0, 1202, 778)), reader.getOriginalGridRange());
+//        assertEquals(new GeneralGridEnvelope(new Rectangle(0.0, 0.0, 0.0, 0.0)), reader.getOriginalEnvelope());
 
         GeneralEnvelope generalEnvelope = new GeneralEnvelope(new double[]{0, 1,}, new double[]{2, 3,});
 

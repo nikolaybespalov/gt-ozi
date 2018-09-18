57569ce5806a07b7e5d00b6569326cc08bd9b341
Nikolay Bespalov <nikolaybespalov@gmail.com>
2018-09-18 03:07:27 +0300

    UTM NAD27 support

--- src/main/java/com/github/nikolaybespalov/gtozi/OziMapFileReader.java
+++ src/main/java/com/github/nikolaybespalov/gtozi/OziMapFileReader.java
@@ -33,6 +33,7 @@ import org.opengis.referencing.datum.GeodeticDatum;
 import org.opengis.referencing.operation.*;
 
 import javax.measure.unit.SI;
+import javax.xml.crypto.Data;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
@@ -278,9 +279,20 @@ final class OziMapFileReader {
             }
 
             if (north) {
-                crs = CRS.decode("EPSG:267" + zone);
+                if ("NAD83".equals(geoCrs.getDatum().getName().getCode())) {
+                    crs = CRS.decode("EPSG:269" + zone);
+                }
+                else if ("NAD27 Central".equals(geoCrs.getDatum().getName().getCode())) {
+                    crs = CRS.decode("EPSG:267" + zone);
+                }
+                else if ("WGS 84".equals(geoCrs.getDatum().getName().getCode())) {
+                    crs = CRS.decode("EPSG:326" + zone);
+                }
+                else {
+                    throw new DataSourceException("Unsupported UTM datum: " + geoCrs.getDatum().getName().getCode());
+                }
             } else {
-                crs = CRS.decode("EPSG:267" + zone);
+                throw new DataSourceException("Southern hemisphere UTM not supported. Sorry. Contact me, please");
             }
         } else {
             String[] values = null;

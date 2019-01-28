21cbf31e0e6a4288e72caba75e36a014facfe11c
Nikolay Bespalov <nikolaybespalov@gmail.com>
2019-01-28 22:36:33 +0300

    Code cleanup

--- src/test/java/com/github/nikolaybespalov/gtozi/GridFormatFinderTest.java
+++ src/test/java/com/github/nikolaybespalov/gtozi/GridFormatFinderTest.java
@@ -5,15 +5,13 @@ import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridFormatFinder;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class GridFormatFinderTest {
     @Test
-    public void testFindFormat() throws Exception {
-        AbstractGridFormat format = GridFormatFinder.findFormat(TestData.file(CrsTest.class, "02-merc/merc-nad27.map"));
+    public void gridFormatFinderFindFormatShouldReturnOziMapFormat() throws Exception {
+        AbstractGridFormat format = GridFormatFinder.findFormat(TestData.file(GridFormatFinderTest.class, "02-merc/merc-nad27.map"));
 
-        assertNotNull(format);
-        assertNotEquals(format.getName(), "Unknown Format");
+        assertTrue(format instanceof OziMapFormat);
     }
 }

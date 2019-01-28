ec6ff020dd43a5580428f27bcfe0c1b86ff5e270
Nikolay Bespalov <nikolaybespalov@gmail.com>
2019-01-28 22:39:07 +0300

    Code cleanup

--- src/test/java/com/github/nikolaybespalov/gtozi/OziMapReaderTest.java
+++ src/test/java/com/github/nikolaybespalov/gtozi/OziMapReaderTest.java
@@ -1,19 +1,12 @@
 package com.github.nikolaybespalov.gtozi;
 
 import org.geotools.TestData;
-import org.geotools.coverage.grid.GridEnvelope2D;
-import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
-import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.data.FileServiceInfo;
-import org.geotools.geometry.GeneralEnvelope;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.opengis.parameter.GeneralParameterValue;
-import org.opengis.parameter.ParameterValue;
 
-import java.awt.*;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,30 +36,8 @@ public class OziMapReaderTest {
     }
 
     @Test
-    public void testRead() throws IOException {
-
-        // Define a GridGeometry in order to reduce the output
-        final ParameterValue<GridGeometry2D> gg =
-                AbstractGridFormat.READ_GRIDGEOMETRY2D.createValue();
-        final GeneralEnvelope envelope = reader.getOriginalEnvelope();
-        final Dimension dim = new Dimension();
-        dim.setSize(
-                reader.getOriginalGridRange().getSpan(0) / 4,
-                reader.getOriginalGridRange().getSpan(1) / 4);
-        final Rectangle rasterArea = ((GridEnvelope2D) reader.getOriginalGridRange());
-        rasterArea.setSize(dim);
-        final GridEnvelope2D range = new GridEnvelope2D(rasterArea);
-        gg.setValue(new GridGeometry2D(range, envelope));
-
-        ParameterValue tileSize = AbstractGridFormat.SUGGESTED_TILE_SIZE.createValue();
-
-        assertNotNull(reader.read(new GeneralParameterValue[]{gg, tileSize}));
-
-        tileSize.setValue("256");
-        assertNotNull(reader.read(new GeneralParameterValue[]{gg, tileSize}));
-
-        tileSize.setValue("128,128,128");
-        assertNotNull(reader.read(new GeneralParameterValue[]{gg, tileSize}));
+    public void readShouldReturnNonNull() throws IOException {
+        assertNotNull(reader.read(null));
     }
 
     @Test
